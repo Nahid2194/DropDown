acff6a8b3ad47daf2e8e2256eeaec7d2d8f402fa
Nahid2194 <nahidjc80@gmail.com>
2021-03-01 15:58:56 +0600

    completed

--- app/src/main/java/com/example/dropdown/MainActivity.java
+++ app/src/main/java/com/example/dropdown/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.dropdown;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -25,11 +29,37 @@ Spinner mySpinner;
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+      String Text=parent.getItemAtPosition(position).toString();
+      Fragment myFragment;
+      if(Text.equals("First")){
+          myFragment = new BdFragment();
 
+      }else if(Text.equals("Second")){
+           myFragment = new InFragment();
+      }else{
+           myFragment = new BlankFragment();
+      }
+      FragmentManager fm = getSupportFragmentManager();
+      FragmentTransaction ft = fm.beginTransaction();
+      ft.replace(R.id.myFragmentID,myFragment);
+      ft.commit();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+        Fragment myFragment = new BlankFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.myFragmentID,myFragment);
+        ft.commit();
+    }
+    public void Button(View view) {
+
+            Intent myintent = new Intent(MainActivity.this,SecondActivity.class);
+            startActivity(myintent);
+
 
     }
+
+
 }
---
+++ app/src/main/java/com/example/dropdown/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.dropdown;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
